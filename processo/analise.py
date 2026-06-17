f9f68f08dda7d9b74b364a7865936c8f60fcfdd6
Matheus Henrique <henriquem320@gmail.com>
2026-06-16 21:38:57 -0300

    main: Atribuindo orientação a objeto e conectando os arquivos.

--- processo/analise.py
+++ processo/analise.py
@@ -3,44 +3,62 @@ import pandas as pd
 
 df = ler_csv()
 
-# coluna item
-def corrige_coluna_item():
+class Analise:
+    def __init__(self, df):
+        self.df = df
+
+    def corrige_coluna_item(self):
     
-    # Buscar o item com menos registros, excluindo 'ERROR' e 'UNKNOWN'
-    valores_validos = df.loc[~df['Item'].isin(['ERROR', 'UNKNOWN']), 'Item']
-    item_menos_frequente = valores_validos.value_counts().idxmin()
+        # Buscar o item com menos registros, excluindo 'ERROR' e 'UNKNOWN'
+        valores_validos = self.df.loc[~self.df['Item'].isin(['ERROR', 'UNKNOWN']), 'Item']
+        item_menos_frequente = valores_validos.value_counts().idxmin()
 
-    # Atribuindo o item menos frequente aos dados com erro
-    df.loc[df['Item'].isin(['ERROR', 'UNKNOWN']), 'Item'] = item_menos_frequente
+        # Atribuindo o item menos frequente aos dados com erro
+        self.df.loc[self.df['Item'].isin(['ERROR', 'UNKNOWN']), 'Item'] = item_menos_frequente
 
-    # Buscando dados nulos
-    item_mais_frequente = df['Item'].value_counts().idxmax()
-    df['Item'].fillna(item_mais_frequente, inplace=True)
+        # Buscando dados nulos
+        item_mais_frequente = self.df['Item'].value_counts().idxmax()
+        self.df['Item'].fillna(item_mais_frequente, inplace=True)
 
-def corrige_coluna_payment_method():
+    def corrige_coluna_payment_method(self):
 
-    # Atribuindo o item mais frequente aos dados com erro
-    pag_mais_frequente = df['Payment Method'].value_counts().idxmax()
-    df.loc[df['Payment Method'].isin(['ERROR', 'UNKNOWN']), 'Payment Method'] = pag_mais_frequente
+        # Atribuindo o item mais frequente aos dados com erro
+        pag_mais_frequente = self.df['Payment Method'].value_counts().idxmax()
+        self.df.loc[self.df['Payment Method'].isin(['ERROR', 'UNKNOWN']), 'Payment Method'] = pag_mais_frequente
 
-    # Buscando dados nulos e atribuindo o item menos frequente a eles
-    pag_menos_frequente = df['Payment Method'].value_counts().idxmin()
-    df['Payment Method'].fillna(pag_menos_frequente, inplace=True)
+        # Buscando dados nulos e atribuindo o item menos frequente a eles
+        pag_menos_frequente = self.df['Payment Method'].value_counts().idxmin()
+        self.df['Payment Method'].fillna(pag_menos_frequente, inplace=True)
 
-def corrige_transaction_date():
-    
-    # Convertendo coluna 'Transaction Date' para datetime e atribuindo para a data atual
-    df['Transaction Date'] = pd.to_datetime(df['Transaction Date'], errors='coerce')
-    df['Transaction Date'] = df['Transaction Date'].fillna(pd.Timestamp.today())
+    def corrige_transaction_date(self):
 
-def corrige_coluna_location():
+        # Convertendo coluna 'Transaction Date' para datetime e atribuindo para a data atual
+        self.df['Transaction Date'] = pd.to_datetime(self.df['Transaction Date'], errors='coerce')
+        self.df['Transaction Date'] = self.df['Transaction Date'].fillna(pd.Timestamp.today())
 
-    # Filtrando apenas os valores válidos, excluindo 'ERROR' e 'UNKNOWN'
-    valor_valido = df.loc[~df['Location'].isin(['ERROR', 'UNKNOWN']), 'Location']
-    local_mais_frequente = valor_valido.value_counts().idxmax()
-    local_menos_frequente = valor_valido.value_counts().idxmin()
+    def corrige_coluna_location(self):
 
-    df.loc[df['Location'] == 'ERROR', 'Location'] = local_mais_frequente
-    df.loc[df['Location'] == 'UNKNOWN', 'Location'] = local_menos_frequente
+        # Filtrando apenas os valores válidos, excluindo 'ERROR' e 'UNKNOWN'
+        valor_valido = self.df.loc[~self.df['Location'].isin(['ERROR', 'UNKNOWN']), 'Location']
+        local_mais_frequente = valor_valido.value_counts().idxmax()
+        local_menos_frequente = valor_valido.value_counts().idxmin()
 
-    df['Location'].fillna('Other', inplace=True)
+        self.df.loc[self.df['Location'] == 'ERROR', 'Location'] = local_mais_frequente
+        self.df.loc[self.df['Location'] == 'UNKNOWN', 'Location'] = local_menos_frequente
+
+        self.df['Location'].fillna('Other', inplace=True)
+
+    def corrige_coluna_quantity(self):
+
+        # convertendo a coluna 'Quantity' para numérica, atribuindo 0 para os erros
+        self.df['Quantity'] = pd.to_numeric(self.df['Quantity'], errors='coerce').fillna(0).astype(int)
+
+    def corrige_coluna_price(self):
+
+        # convertendo a coluna 'Price Per Unit' para float, atribuindo 0 para os erros
+        self.df['Price Per Unit'] = pd.to_numeric(self.df['Price Per Unit'], errors='coerce').fillna(0.0)
+
+    def corrige_coluna_total_spent(self):
+
+        # convertendo a coluna 'Total Spent' para float, atribuindo 0 para os erros
+        self.df['Total Spent'] = pd.to_numeric(self.df['Total Spent'], errors='coerce').fillna(0.0)
---
+++ processo/main.py
@@ -0,0 +1,18 @@
+from dados import ler_csv
+from analise import Analise
+from processo import analise
+
+def main():
+    df = ler_csv()
+    analise = Analise(df)
+
+    analise.corrige_coluna_item()
+    analise.corrige_coluna_payment_method()
+    analise.corrige_transaction_date()
+    analise.corrige_coluna_location()
+    analise.corrige_coluna_quantity()
+    analise.corrige_coluna_price()
+    analise.corrige_coluna_total_spent()
+
+if __name__ == "__main__":
+    main()
